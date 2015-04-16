60d072852923d2f405bb99f287f69fed878eaba6
lemoineffs <lemoineffs@gmail.com>
2015-04-16 20:27:33 +0200

    bla

--- NetworkProject/src/nog/Server.java
+++ NetworkProject/src/nog/Server.java
@@ -14,7 +14,7 @@ public class Server {
                 boolean listening = true;
                 ServerSocket serverSocket = null;
        /*****************
-        * JAG LA TILL DEN HÄR KOMMENTAREN FÖR ATT TESTA GIT.
+        * JAG LA TILL DEN HÄR KOMMENTAREN FÖR ATT TESTA GIT.ghjkyuh
         */
         try {
             serverSocket = new ServerSocket(5555);
